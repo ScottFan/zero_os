2864269cda1a7c10274ec300547e626fe366a44f
ScottFan <22972998@qq.com>
2021-11-06 00:27:43 +0800

    add println to main.rs

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # zero_os
 该项目为个人学习项目，使用Rust语言编写的可以在裸机上运行的程序，目标平台为x86_64位CPU
 
+编译：cargo xbuild
+
+创建镜像：cargo bootimage 成功后生成bootimage-zero_os.bin
+
 运行方式 qemu-system-x86_64 -drive format=raw,file=bootimage-zero_os.bin
 
 ![图片](https://user-images.githubusercontent.com/6795484/139184792-0b81f294-d3b0-4435-9089-270481467238.png)
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
+#![feature(core_intrinsics)]
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+#[macro_use]
+extern crate mars_vga;
+ 
+use core::intrinsics;
+use core::fmt::{self, Write};
 use core::panic::PanicInfo;
 
+ 
 // This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello everyone! \n Welcome to Zero";
+static HELLO: &[u8] = b"Hello everyone!  Welcome to Zero";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     let vga_buffer = 0xb8000 as *mut u8;
-
+    println!("Zero OS\n");
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
